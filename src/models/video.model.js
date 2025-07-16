218c15a77c879751f890623f299ad6f3cf9dd26d
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-07-16 05:53:14 +0500

    added aggregatePaginate plugin

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, { mongo, Schema } from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
@@ -39,6 +39,8 @@ const videoSchema = new Schema(
         timestamps: true
     }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 
 export const Video = mongoose.model('Video', videoSchema)
