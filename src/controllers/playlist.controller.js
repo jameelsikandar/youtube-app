8d8f4599f7f46a220124135aea32a0f8554e4784
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-01 23:26:28 +0500

    added update playlist plus addVideosToPlaylist ctrler

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -5,7 +5,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 
-
+// create playlist
 const createPlaylist = asyncHandler(async (req, res) => {
     const { title, descripition } = req.body;
     const userId = req.user._id;
@@ -28,4 +28,74 @@ const createPlaylist = asyncHandler(async (req, res) => {
         )
 });
 
-export { createPlaylist }
+//  update playlist
+const updatePlaylist = asyncHandler(async (req, res) => {
+    const { title, descripition } = req.body;
+    const { playlistId } = req.params;
+
+    if (!title) {
+        throw new ApiError(400, "Title is required!")
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(playlistId)) {
+        throw new ApiError(400, "Playlist id is invalid")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(400, "Playlist not found!")
+    }
+
+    if (playlist.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(400, "You can only update your own playlists!")
+    }
+
+
+    playlist.title = title;
+    playlist.descripition = descripition;
+
+    const updatedPlaylist = await playlist.save()
+
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedPlaylist, "Playlist Updated successfully!"))
+});
+
+// add videos to playlist
+const addVideosToPlaylist = asyncHandler(async (req, res) => {
+    const { playlistId, videoId } = req.params;
+
+    if (!(mongoose.Types.ObjectId.isValid(playlistId) && mongoose.Types.ObjectId.isValid(videoId))) {
+        throw new ApiError(400, "Plasylist id or video id is invalid!")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(400, "Playlist not found")
+    }
+
+    if (playlist.videos.includes(videoId)) {
+        throw new ApiError(400, "Video already exists in playlist!")
+    }
+
+    playlist.videos.push(videoId)
+
+    const addedVideo = await playlist.save();
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, addedVideo, "Video added successfully!"))
+
+})
+
+
+
+
+export {
+    createPlaylist,
+    updatePlaylist,
+    addVideosToPlaylist
+}
--- src/routes/playlist.route.js
+++ src/routes/playlist.route.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { createPlaylist } from '../controllers/playlist.controller.js';
+import { createPlaylist, updatePlaylist, addVideosToPlaylist } from '../controllers/playlist.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 const router = Router();
@@ -8,5 +8,10 @@ const router = Router();
 // create playlist
 router.route('/create-playlist').post(verifyJWT, createPlaylist);
 
+// update playlsit
+router.route('/update-playlist/:playlistId').post(verifyJWT, updatePlaylist);
+
+// add video to playlist
+router.route('/add-video/:playlistId/:videoId').post(verifyJWT, addVideosToPlaylist)
 
 export default router;
